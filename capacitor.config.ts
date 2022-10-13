5450db1a36ed67a78f3f7a291d567b99d48a9f50
unknown <FarhanRovi123>
2022-10-13 08:04:42 +0700

    initial project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tugasionic_install',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
