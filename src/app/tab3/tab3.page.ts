66c62880092f2d88daf9e071a9771a8f657c61c4
salimsea <salimajah01@gmail.com>
2022-10-13 12:18:48 +0700

    add daftar nilai

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+// Import Add (before/after import please installation axios from cmd)
+import axios from 'axios';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +11,23 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  // Variabel GetData for array
+  public allData:any = [];
+ 
+ 
+  constructor() {
+
+    // Form Load GetData
+    this.GetData();
+  }
+
+
+  async GetData(){
+    const res:any = await axios.get('http://mobileprogramming123.000webhostapp.com/get_data.php');
+
+    console.log(res.data);
+    this.allData = res.data.result;
+  }
+
 
 }
